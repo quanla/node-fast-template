235ede2f2d2ec4cdccca18ef9b6eb16a0a136b71
Quan Le <leanhquan@prototype1.io>
2016-05-19 10:57:19 +0700

    Minor

--- filter.js
+++ filter.js
@@ -3,14 +3,12 @@ var filters = {
         if (typeof obj == "number") {
             obj = new Date(obj);
         }
-        console.log(obj);
         return DateUtil.format(obj, format)
     }
 };
 
 module.exports = function(exp) {
     return function(mainVal, context) {
-        console.log(context.post.slug);
         return filters["date"](mainVal, "dd MMM yyyy");
     };
 };
